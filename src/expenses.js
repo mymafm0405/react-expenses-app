854440c89e5570fbc4d2487aee30746bb770e069
Mahmoud <mymafm0405@gmail.com>
2021-12-09 11:46:23 +0300

    add more components

--- src/expenses.js
+++ src/expenses.js
@@ -1,14 +1,15 @@
 import "./expenses.css";
 import ExpenseItem from "./expense-item";
+import Card from "./card";
 
 function Expenses({ expenses }) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem expense={expenses[0]} />
       <ExpenseItem expense={expenses[1]} />
       <ExpenseItem expense={expenses[2]} />
       <ExpenseItem expense={expenses[3]} />
-    </div>
+    </Card>
   );
 }
 
